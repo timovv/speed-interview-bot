d1148816f0a542a9d8eb228669881572a7021fca
Timo van Veenendaal <me@timo.nz>
2020-09-02 15:39:20 +1200

    Extract bot to separate file from index

---
+++ src/bot.ts
@@ -0,0 +1,83 @@
+import Discord, { Client } from "discord.js";
+import { handleCommand } from "./commands";
+import { enqueueUser, getQueue, updateQueue, dequeueUser } from "./queue";
+import { getSettings } from "./settings";
+
+export const startBot = async (token: string): Promise<Client> => {
+  const client = new Discord.Client();
+
+  client.on("voiceStateUpdate", async (previous, next) => {
+    const previousSettings = getSettings(previous.guild);
+    const nextSettings = getSettings(next.guild);
+
+    // add to waiting room
+    if (
+      previous.channel?.name !== nextSettings.waitingRoomName &&
+      next.channel?.name === nextSettings.waitingRoomName &&
+      next.member &&
+      !next.member.roles.cache.some(
+        (x) =>
+          x.name === nextSettings.adminRole ||
+          x.name === nextSettings.interviewerRole
+      )
+    ) {
+      const queue = getQueue(next.guild);
+      const [newQueue, position] = enqueueUser(queue, next.member.id);
+      updateQueue(next.guild, newQueue);
+      const dm = await next.member.createDM();
+      await dm.send(
+        `Welcome to the waiting room. You have been queued up and are now in position #${position}. To leave the queue, leave the waiting room at any time.`
+      );
+      return;
+    }
+
+    // remove from waiting room
+    if (
+      previous.channel?.name === previousSettings.waitingRoomName &&
+      next.channel?.name !== previousSettings.waitingRoomName &&
+      previous.member &&
+      getQueue(next.guild).queuedUsers.includes(previous.member.id)
+    ) {
+      const queue = getQueue(next.guild);
+      updateQueue(next.guild, dequeueUser(queue, previous.member.id));
+      const dm = await previous.member.createDM();
+      await dm.send(
+        `You have been removed from the queue. Thanks for coming along!`
+      );
+      return;
+    }
+  });
+
+  const setup = (guild: Discord.Guild) => {
+    console.log(`Joined guild ${guild.name}`);
+    const { interviewerRole, adminRole } = getSettings(guild);
+
+    const waitingRoomChannel = guild.channels.cache.find(
+      (channel) => channel.name === getSettings(guild).waitingRoomName
+    );
+    if (waitingRoomChannel) {
+      waitingRoomChannel.members
+        .filter(
+          (member) =>
+            !member.roles.cache.some((x) =>
+              [interviewerRole, adminRole].includes(x.name)
+            )
+        )
+        .forEach((member) =>
+          updateQueue(guild, enqueueUser(getQueue(guild), member.id)[0])
+        );
+    }
+  };
+
+  client.on("guildCreate", setup);
+
+  client.on("message", handleCommand);
+
+  client.on("ready", () => {
+    console.log("Bot is ready!");
+    client.guilds.cache.forEach((guild) => setup(guild));
+  });
+
+  await client.login(token);
+  return client;
+};
--- src/index.ts
+++ src/index.ts
@@ -1,86 +1,13 @@
-import Discord from "discord.js";
 import dotenv from "dotenv";
-import { handleCommand } from "./commands";
-import { enqueueUser, getQueue, updateQueue, dequeueUser } from "./queue";
-import { getSettings } from "./settings";
+import { startBot } from "./bot";
+
 dotenv.config();
 
-const client = new Discord.Client();
-
-client.on("voiceStateUpdate", async (previous, next) => {
-  const previousSettings = getSettings(previous.guild);
-  const nextSettings = getSettings(next.guild);
-
-  // add to waiting room
-  if (
-    previous.channel?.name !== nextSettings.waitingRoomName &&
-    next.channel?.name === nextSettings.waitingRoomName &&
-    next.member &&
-    !next.member.roles.cache.some(
-      (x) =>
-        x.name === nextSettings.adminRole ||
-        x.name === nextSettings.interviewerRole
-    )
-  ) {
-    const queue = getQueue(next.guild);
-    const [newQueue, position] = enqueueUser(queue, next.member.id);
-    updateQueue(next.guild, newQueue);
-    const dm = await next.member.createDM();
-    await dm.send(
-      `Welcome to the waiting room. You have been queued up and are now in position #${position}. To leave the queue, leave the waiting room at any time.`
-    );
-    return;
-  }
-
-  // remove from waiting room
-  if (
-    previous.channel?.name === previousSettings.waitingRoomName &&
-    next.channel?.name !== previousSettings.waitingRoomName &&
-    previous.member &&
-    !previous.member.roles.cache.some(
-      (x) =>
-        x.name === previousSettings.adminRole ||
-        x.name === previousSettings.interviewerRole
-    )
-  ) {
-    const queue = getQueue(next.guild);
-    updateQueue(next.guild, dequeueUser(queue, previous.member.id));
-    const dm = await previous.member.createDM();
-    await dm.send(
-      `You have been removed from the queue. Thanks for coming along!`
-    );
-    return;
-  }
-});
-
-const setup = (guild: Discord.Guild) => {
-  console.log(`Joined guild ${guild.name}`);
-  const { interviewerRole, adminRole } = getSettings(guild);
-
-  const waitingRoomChannel = guild.channels.cache.find(
-    (channel) => channel.name === getSettings(guild).waitingRoomName
+if (!process.env["DISCORD_TOKEN"]) {
+  console.error(
+    "DISCORD_TOKEN is not set. Configure the environment variable or set it in .env."
   );
-  if (waitingRoomChannel) {
-    waitingRoomChannel.members
-      .filter(
-        (member) =>
-          !member.roles.cache.some((x) =>
-            [interviewerRole, adminRole].includes(x.name)
-          )
-      )
-      .forEach((member) =>
-        updateQueue(guild, enqueueUser(getQueue(guild), member.id)[0])
-      );
-  }
-};
+  process.exit(1);
+}
 
-client.on("guildCreate", setup);
-
-client.on("message", handleCommand);
-
-client.on("ready", () => {
-  console.log("Bot is ready!");
-  client.guilds.cache.forEach((guild) => setup(guild));
-});
-
-client.login(process.env["DISCORD_TOKEN"]);
+startBot(process.env["DISCORD_TOKEN"]);
